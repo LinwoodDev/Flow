93c976055d64bd265387ae7bc47f0be43674135e
CodeDoctorDE <codedoctor@linwood.dev>
2022-10-30 20:56:31 +0100

    remove unused dependencies

--- app/linux/flutter/generated_plugins.cmake
+++ app/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  biometric_storage
   sqlite3_flutter_libs
   url_launcher_linux
 )
--- app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import biometric_storage
 import path_provider_macos
 import shared_preferences_macos
 import sqflite
@@ -13,7 +12,6 @@ import sqlite3_flutter_libs
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
